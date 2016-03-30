8f2daaefd03e9a3b3a8be97c718e2b19d45d6c82
zwc_1122@163.com <zwc_1122@163.com>
2016-03-30 18:29:52 +0800

    libeventc++11 封装

---
+++ libeventc++11/example/index.h
@@ -0,0 +1,19 @@
+/*
+ * index.h
+ *
+ *  Created on: 2015年6月27日
+ *      Author: havesnag
+ */
+
+#ifndef EXAMPLE_INDEX_H_
+#define EXAMPLE_INDEX_H_
+
+void timerExample();
+
+void asyncExample();
+
+void tcpExample();
+
+void httpExample();
+
+#endif /* EXAMPLE_INDEX_H_ */
